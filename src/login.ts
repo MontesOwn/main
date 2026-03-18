ac572b4d29f830a0d09b815a1bc1cd76e80fdec5
Ryan Montville <ryan.a.montville@gmail.com>
2026-03-17 23:38:53 -0500

    fixing user auth on mobile

--- src/login.ts
+++ src/login.ts
@@ -1,10 +1,33 @@
 import { initializeApp } from "./main";
 import { signInWithGooglePopup } from "./firebase/authService";
-import { storeMessage } from "./modules/utils";
+import { createMessage, storeMessage } from "./modules/utils";
 import { navigateTo } from "./modules/navigate";
 
 initializeApp("Monte's Own", "Log in").then(async () => {
-    let result = await signInWithGooglePopup();
-    storeMessage(`Welcom ${result["user"]["displayName"]}`, "main-message", "check_circle");
-    navigateTo("/index");
+    createMessage("Opening Google window...", "main-message", "info");
+    try {
+        const result = await signInWithGooglePopup();
+        //If sucessful sign in with Google, close the modal and display the message
+        const user = result.user;
+        if (user) {
+            storeMessage(
+                `Welcome ${user.displayName}`,
+                "main-message",
+                "check_circle",
+            );
+            navigateTo("/index");
+        }
+    } catch (error: any) {
+        let errorMessage = "Sign-In failed.";
+        if (error.code === "auth/popup-closed-by-user") {
+            errorMessage = "Sign-In window closed.";
+        } else if (error.code === "auth/cancelled-popup-request") {
+            errorMessage = "Sign-In request already in progress.";
+        } else {
+            errorMessage = `Error: ${error.message}`;
+        }
+        createMessage(errorMessage, "main-message", "error");
+        console.error("Google sign-in error details:", error);
+    }
+
 });
